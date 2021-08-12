0bd26b18aff10e08a94c6469a2bedbb2a4c9aa5f
Leandro Martinez <leandro@iqm.unicamp.br>
2021-08-12 16:58:37 -0300

    added bounds

--- src/Polyuretanes.jl
+++ src/Polyuretanes.jl
@@ -1,13 +1,15 @@
 module Polyuretanes
 
+using Plots, Plots.Measures
 using Catalyst
 using DifferentialEquations
 using DelimitedFiles
+using Printf
 
 export fit
+export fitall
 export simulate
-export plot_OH
-export plot_all
+export plot
 
 function score(sim, experimental_data, sim_col)
     p = getindex.(sim.u, sim_col)
@@ -16,12 +18,12 @@ function score(sim, experimental_data, sim_col)
         i = findfirst(ts -> ts > t, sim.t)
         dpdt = (p[i] - p[i - 1]) / (sim.t[i] - sim.t[i - 1])
         pred_at_t = p[i - 1] + dpdt * (t - sim.t[i - 1]) 
-        score += (val - pred_at_t)^2
+        score += ((val - pred_at_t)/val)^2
     end
     return score / (size(experimental_data, 1))
 end
 
-function optimizer(x, f;max_failed_trials=10000,showprogress=false)
+function optimizer(x, f, lower, upper;opt=FitOptions())
     xbest = copy(x)
     xtrial = copy(x)
     failed_trials = 0
@@ -30,15 +32,21 @@ function optimizer(x, f;max_failed_trials=10000,showprogress=false)
     fbest = f(x)
     itrial = 0
     irand = 1
-    while failed_trials < max_failed_trials
+    if opt.showprogress
+        @printf("Trial: %10i fbest = %12.5e step = %12.5e", itrial, fbest, step)
+    end
+    while failed_trials < opt.max_failed_trials || itrial < opt.max_initial_trials
         itrial += 1
         xtrial[irand] = xbest[irand] + -step * xbest[irand] + (2 * rand() * step) * xbest[irand]
+        xtrial[irand] = max(lower[irand],min(upper[irand],xtrial[irand]))
         ftrial = f(xtrial)
         if ftrial < fbest
             failed_trials = 0
             xbest .= xtrial
             fbest = ftrial
-            showprogress && println("Trial $itrial: fbest = $fbest step = $step")
+            if opt.showprogress
+              @printf("\rTrial: %10i fbest = %12.5e step = %12.5e", itrial, fbest, step)
+            end
             step = 1.
         else 
             irand = rand(1:length(x))
@@ -46,6 +54,7 @@ function optimizer(x, f;max_failed_trials=10000,showprogress=false)
             failed_trials += 1
         end
     end
+    opt.showprogress && @printf("\n")
     return fbest, xbest
 end
 
@@ -92,61 +101,66 @@ function Base.show(io::IO, r::Result)
     end
 end
 
-function plot_OH(result;title=nothing) 
-    plot = Main.plot
-    plot! = Main.plot!
-    scatter! = Main.scatter!
-    scalefontsizes = Main.scalefontsizes
-    default = Main.default
+function plot(
+    result::Result,
+    species="OH"::String;
+    labels=["Simulation","Experimental"],
+    xlabel="time / hours",
+    ylabel="[$species] / mol/L",
+    title="",
+    tscale=3600
+) 
+
+    icol = findfirst(x -> String(x) == species, keys(result.sys.c0))
   
     sim = result.sim
     experimental_data = result.sys.experimental_data
     default(
         fontfamily="Computer Modern",
-        linewidth=2, framestyle=:box, label=nothing, grid=false
+        linewidth=2, framestyle=:box, label=nothing, grid=false,
+        margin=5mm
     )
     scalefontsizes()
     scalefontsizes(1.2)
-  	plt = plot()
+  	plt = Plots.plot()
   	plot!(plt,
-  	  	sim.t,getindex.(sim.u, 4),
-  	  	xlabel="time / s",
-  	  	ylabel="[OH] / mol/L",
-  	  	label="Simulation"
+  	  	sim.t/tscale,
+        getindex.(sim.u, icol),
+  	  	xlabel=xlabel,
+  	  	ylabel=ylabel,
+  	  	label=labels[1]
   	)
   	scatter!(plt,
-  		experimental_data[:,1],
+  		experimental_data[:,1]/tscale,
   		experimental_data[:,2],
-  		label="Experimental"
+  		label=labels[2]
   	)
     plot!(plt, title=title)
   	return plt
 end
 
-function plot_all(
+function plot(
     results::AbstractVector{Result};
+    species="OH",
     title="",
-    col=4,
-    ylabel="[OH] / mol/L",
+    ylabel="[$species] / mol/L",
     xlabel="time / hours",
     tscale=3600
 )
-    plot = Main.plot
-    plot! = Main.plot!
-    scatter! = Main.scatter!
-    scalefontsizes = Main.scalefontsizes
-    default = Main.default
+    icol = findfirst(x -> String(x) == species, keys(results[1].sys.c0))
+
     default(
         fontfamily="Computer Modern",
-        linewidth=2, framestyle=:box, label=nothing, grid=false
+        linewidth=2, framestyle=:box, label=nothing, grid=false,
+        margin=5mm
     )
     scalefontsizes()
     scalefontsizes(1.2)
 
-    plt = plot()
+    plt = Plots.plot()
     for (i, r) in pairs(results)
     sim = r.sim
-        plot!(plt, sim.t / tscale, getindex.(sim.u, col), label=r.sys.label, color=i)
+        plot!(plt, sim.t / tscale, getindex.(sim.u, icol), label=r.sys.label, color=i)
         scatter!(plt, r.sys.experimental_data[:,1] / tscale, r.sys.experimental_data[:,2], color=i)
     end
     plot!(plt, title=title)
@@ -161,6 +175,8 @@ Base.@kwdef struct ReactionSystem{C,P,T}
     experimental_data::Matrix{Float64}
     c0::C
     p0::P
+    lower::P
+    upper::P
     tspan::T
 end
 
@@ -171,7 +187,9 @@ function datasets(dir="/home/leandro/.julia/dev/Polyuretanes/data")
         label="NCO/DIPA = 1",
         experimental_data=readdlm("$dir/bd_ipdi_110C_1.0.dat"),
         c0=(U = 6.68, NCO = 0., DIPA_l = 0., OH = 6.68, POL = 0., DIPA_v = 0.),
-        p0=(k1 = 6.8e-6, km1 = 4.5e-4, k2 = 7.7e-5, A = 1.13e-6, b = 0.5),
+        p0=(k1 = 6.8e-6, km1 = 4.5e-4, k2 = 2.2e-4, A = 1.13e-6, b = 0.5),
+        lower=(k1 = -Inf, km1 = -Inf, k2 = 1.7e-4, A = -Inf, b = -Inf),
+        upper=(k1 = +Inf, km1 = +Inf, k2 = 2.7e-4, A = +Inf, b = +Inf),
         tspan=(0., 350e3)
     )
   
@@ -181,6 +199,8 @@ function datasets(dir="/home/leandro/.julia/dev/Polyuretanes/data")
         experimental_data=readdlm("$dir/bd_ipdi_110C_0.5.dat"),
         c0=(U = 3.34, NCO = 3.34, DIPA_l = 0.,	OH = 6.68, POL = 0., DIPA_v = 0.),
         p0=(k1 = 6.8e-6, km1 = 4.5e-4, k2 = 7.7e-5, A = 1.13e-6, b = 0.5),
+        lower=(k1 = -Inf, km1 = -Inf, k2 = 1.7e-4, A = -Inf, b = -Inf),
+        upper=(k1 = +Inf, km1 = +Inf, k2 = 2.7e-4, A = +Inf, b = +Inf),
         tspan=(0., 350e3)
     )
       
@@ -190,6 +210,8 @@ function datasets(dir="/home/leandro/.julia/dev/Polyuretanes/data")
         experimental_data=readdlm("$dir/bd_ipdi_110C_0.25.dat"),
         c0=(U = 1.67, NCO = 5.01, DIPA_l = 0.,	OH = 6.68, POL = 0., DIPA_v = 0.),
         p0=(k1 = 6.8e-6, km1 = 4.5e-4, k2 = 7.7e-5, A = 1.13e-6, b = 0.5),
+        lower=(k1 = -Inf, km1 = -Inf, k2 = 1.7e-4, A = -Inf, b = -Inf),
+        upper=(k1 = +Inf, km1 = +Inf, k2 = 2.7e-4, A = +Inf, b = +Inf),
         tspan=(0., 350e3)
     ) 
       
@@ -211,19 +233,24 @@ function objective_function(x, odes, systems, sim_col)
     return f / length(systems)
 end
 
-fit(
-    reaction=reaction1(), 
-    system::ReactionSystem=datasets()[1],
-    max_failed_trials=10000, showprogress=true
-) = 
-    fit(reaction, [system], max_failed_trials=max_failed_trials, showprogress=showprogress)
+Base.@kwdef struct FitOptions
+    max_failed_trials::Int = 10_000
+    max_initial_trials::Int = 100_000
+    showprogress::Bool = true
+end
+
+fit(reaction, 
+    system::ReactionSystem;
+    opt=FitOptions()
+) = fit(reaction, [system], opt=opt)
 
 function fit(
-    reaction=reaction1(),
-    systems=datasets("/home/leandro/.julia/dev/Polyuretanes/data");
-    max_failed_trials=10000,showprogress=true
+    reaction,
+    systems::Vector{<:ReactionSystem};
+    opt=FitOptions()
 )
 
+    println("Building differential equation system... ")
     odes = []
     for system in systems
         c0 = [values(system.c0)...]
@@ -236,13 +263,18 @@ function fit(
         push!(odes, ode_problem)
     end
     x0 = Float64[]
+    lower = Float64[]
+    upper = Float64[]
     for par in params(reaction)
         push!(x0, getfield(systems[1].p0, Symbol(par)))
+        push!(lower, getfield(systems[1].lower, Symbol(par)))
+        push!(upper, getfield(systems[1].upper, Symbol(par)))
     end
     @. x0 = x0 + -x0 + rand() * x0
 
+    println("Setting up objective function... ")
     f(x) = objective_function(x, odes, systems, 4)
-    fbest, xbest = optimizer(x0, f, max_failed_trials=max_failed_trials, showprogress=showprogress)
+    fbest, xbest = optimizer(x0, f, lower, upper, opt=opt)
 
     r = Result[]
     for system in systems
@@ -259,5 +291,28 @@ function fit(
     return r, fbest
 end
 
+function fitall(
+    reaction=reaction1(),
+    systems::Vector{<:ReactionSystem}=datasets("/home/leandro/.julia/dev/Polyuretanes/data");
+    opt=FitOptions()
+)
+    # Fit each system independently
+    individual_results = []
+    for system in systems
+        println("----------------------------------------------")
+        println("Fitting system $(system.title)...")
+        println("----------------------------------------------")
+        r = fit(reaction,system,opt=opt)
+        push!(individual_results, r[1])
+    end
+
+    # Optimal overal fit
+    println("----------------------------------------------")
+    println("Fitting all systems...")
+    println("----------------------------------------------")
+    r = fit(reaction,systems,opt=opt)
+
+    return individual_results, r
+end
 
 end # module
